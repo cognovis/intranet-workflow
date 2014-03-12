d4bdfe8d75724adbdc65090bed2f781856f98d9a
Malte Sussdorff <malte.sussdorff@cognovis.com>
2014-03-12 13:22:03 +0100

    Update for approval of HR absences
    Signed-off-by: Malte Sussdorff <malte.sussdorff@cognovis.com>

--- sql/postgresql/workflow-vacation_approval_wf-create.sql
+++ sql/postgresql/workflow-vacation_approval_wf-create.sql
@@ -571,3 +571,52 @@ values
 
 commit;
 
+-- Unassigned callback that assigns the transition to the supervisor of the owner
+-- of the underlying absence
+--
+create or replace function im_workflow__assign_to_absence_supervisor (integer, text)
+returns integer as '
+declare
+	p_task_id		alias for $1;
+	p_custom_arg		alias for $2;
+	v_case_id		integer;	v_object_id		integer;
+	v_creation_user		integer;	v_creation_ip		varchar;
+	v_journal_id		integer;	v_object_type		varchar;
+	v_owner_id		integer;	v_owner_name		varchar;
+	v_supervisor_id		integer;	v_supervisor_name	varchar;
+	v_transition_key	varchar;
+	v_str			text;
+	row			RECORD;
+begin
+	-- Get information about the transition and the "environment"
+	select	tr.transition_key, t.case_id, c.object_id, ua.owner_id, o.creation_ip, o.object_type
+	into	v_transition_key, v_case_id, v_object_id, v_owner_id, v_creation_ip, v_object_type
+	from	wf_tasks t, wf_cases c, wf_transitions tr, im_user_absences ua, acs_objects o
+	where	t.task_id = p_task_id
+		and t.case_id = c.case_id
+		and o.object_id = t.object_id
+        and ua.absence_id = o.object_id
+		and t.workflow_key = tr.workflow_key
+		and t.transition_key = tr.transition_key;
+
+	select	im_name_from_user_id(e.employee_id), 
+		e.supervisor_id, im_name_from_user_id(e.supervisor_id)
+	into  v_owner_name, 
+		v_supervisor_id, v_supervisor_name
+	from	im_employees e
+	where	e.employee_id = v_owner_id;
+
+	IF v_supervisor_id is not null THEN
+		v_journal_id := journal_entry__new(
+		    null, v_case_id,
+		    v_transition_key || '' assign_to_supervisor '' || v_supervisor_name,
+		    v_transition_key || '' assign_to_supervisor '' || v_supervisor_name,
+		    now(), v_creation_user, v_creation_ip,
+		    ''Assigning to '' || v_supervisor_name || '', the supervisor of '' || v_owner_name || ''.''
+		);
+		PERFORM workflow_case__add_task_assignment(p_task_id, v_supervisor_id, ''f'');
+		PERFORM workflow_case__notify_assignee (p_task_id, v_supervisor_id, null, null, 
+			''wf_'' || v_object_type || ''_assignment_notif'');
+	END IF;
+	return 0;
+end;' language 'plpgsql';
